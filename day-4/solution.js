a464939a36037fa1227f771e774457f12fc94f0e
Oliver Weiler <wo6980@googlemail.com>
2016-03-23 09:53:27 +0100

    Update solution.js

--- day-4/solution.js
+++ day-4/solution.js
@@ -3,7 +3,7 @@
 const crypto = require('crypto')
 const argv = require('yargs').default('p', '00000').string('p').argv
 
-for (const n of naturals()) {
+for (let n of naturals()) {
   if (crypto.createHash('md5').update(`ckczppom${d}`).digest('hex').startsWith(argv.p)) {
     console.log(n)
     process.exit()
